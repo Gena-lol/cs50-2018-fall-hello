35f0c465c4d426eea8da390fc40e4e94f4fbfbc5
Gena-lol <105130081+Gena-lol@users.noreply.github.com>
2022-05-15 00:06:53 +0300

    Add files via upload

---
+++ mario_more.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int h;
+    // Prompt user for the height 
+    do 
+    {
+    h = get_int("Please enter the h: ");
+    }    
+    while (h<1 || h>8);
+            // Start graphics
+     for (int i = 1; i <= h; i++)
+     {
+        for (int s=h-i; s>0; s--)
+        {
+             printf(" ");
+        }
+        for (int t=1; t<=i; t++)
+        {
+            printf("#");
+        }
+        // insert spaces and reverse pyramid
+        printf("  ");
+        for (int t=1; t<=i; t++)
+        {
+            printf("#");
+        }
+        printf("\n");
+     }
+     return 0;
+}
